728a3a33deaa0a44df15ee9fb3943fec15c2befa
shruti <shruti4gopalabhatla@gmail.com>
2025-10-16 21:11:50 -0700

    Add objective function to evaluate task performance

--- src/objectivefunc.py
+++ src/objectivefunc.py
@@ -1,1 +1,5 @@
-
+def objective_function(task_accuracy, total_cost, agent_count, 
+                       accuracy_weight=0.5, cost_weight=0.3, minimality_weight=0.2):
+    return (accuracy_weight * task_accuracy) - \
+           (cost_weight * total_cost) - \
+           (minimality_weight * agent_count)
